867d143a3a00548fb1559ce81605db87fccb55fd
diegof <diegof@ea6c44dd-32d5-4dfe-a4a7-25d6a9a78781>
2012-03-27 23:59:53 +0000

    added demo
    git-svn-id: svn+ssh://svn.corp.yahoo.com/yahoo/searchfe/shaker/trunk@35 ea6c44dd-32d5-4dfe-a4a7-25d6a9a78781

---
+++ demo/mojits/secondary/controller.server.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2011 Yahoo! Inc. All rights reserved.
+ */
+YUI.add('secondary', function(Y, NAME) {
+
+/**
+ * The features module.
+ *
+ * @module features
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.mojito.controllers[NAME] = {
+        init: function(config) {
+           this.config = config;
+         },
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            var dim = ac.params.getFromUrl();
+            
+            ac.assets.addCss('/static/' + NAME + '/assets/css/common/' + NAME + '.css');
+            
+            dim.device && ac.assets.addCss(this.cssPath('device',   dim.device));
+            dim.region && ac.assets.addCss(this.cssPath('region', dim.region));
+            dim.skin   && ac.assets.addCss(this.cssPath('skin',   dim.skin));
+
+            ac.done();
+        },
+        
+        cssPath: function (dim, val) {
+            val || (val = dim);
+            
+            return '/static/' + NAME + '/assets/css/' + dim + '/' + val + '/' + 
+                NAME + '-' + val + '.css';
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito']});
