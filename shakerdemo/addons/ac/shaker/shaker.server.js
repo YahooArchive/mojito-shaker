35462e50bb41ea28d3eb6ec2db7ae6e89d728f44
diegof <diegof@ea6c44dd-32d5-4dfe-a4a7-25d6a9a78781>
2012-03-28 02:58:31 +0000

    added addon
    git-svn-id: svn+ssh://svn.corp.yahoo.com/yahoo/searchfe/shaker/trunk@40 ea6c44dd-32d5-4dfe-a4a7-25d6a9a78781

---
+++ shakerdemo/addons/ac/shaker/shaker.server.js
@@ -0,0 +1,143 @@
+YUI.add('mojito-shaker-addon', function(Y, NAME) {
+    var libfs = require('fs'),libpath = require('path');
+
+    function filterAssets(appConfig,list){
+            var app = appConfig,
+                regularExp = '/' + app.prefix +'/([^/]+)/.*',
+                groups = {
+                    app:[],
+                    core:[],
+                    mojits:[]
+                };
+            for(var i = 0 ; i<list.length; i++){
+                var item = list[i],
+                    matched = item.match(regularExp),
+                    type = matched && matched.pop();
+                if(type){
+                    switch(type){
+                        case app.frameworkName: groups.core.push(matched.pop()); break;
+                        case app.appName: groups.app.push(matched.pop());break;
+                        default: groups.mojits.push(matched.pop());break;
+                    }
+                }
+            }
+            return groups;
+    }
+
+    function ShakerAddon(command, adapter, ac) {
+        this._ac = ac;// the future action context of the mojit (not attached yet if mojit created dynamically)
+        this._adapter = adapter;// where the functions done and error live before attach them to the ac.
+        this._command = command;//all the configuration for the mojit
+        this._init(ac);
+    }
+
+    ShakerAddon.prototype = {
+        namespace: 'shaker',
+
+        _init:function(ac){
+            this._app = this._setAppConfig(ac);
+            this._meta = YUI._mojito._cache.shaker.meta;
+        },
+        _setAppConfig: function(ac){
+            var app = ac.app.config.staticHandling || {};
+            app.appName = app.appName || libpath.basename(process.cwd());
+            app.frameworkName = app.frameworkName || 'mojito';
+            app.prefix = app.prefix || 'static';
+            return app;
+        },
+        _matchDimensions :function(selector,dimensions,action,shaken){
+            var parts = selector.split('-'),
+                matched = '', aux;
+                    while(parts.length){
+                        aux = parts.shift();
+                        if(aux === 'common'){
+                            matched+= matched ? '-' + aux : aux;
+                        }else if(aux === 'action'){
+                            matched+= matched? '-' + action: action;
+                        }else{// common case dimensions
+                            if(typeof dimensions[aux] !== 'undefined' && shaken[matched + '-' + dimensions[aux] ]){
+                                matched+= matched ? '-' + dimensions[aux] : dimensions[aux];
+                            }else{
+                                break;
+                            }
+                        }
+                    }
+                    return matched;
+        },
+        _matchMojitDimensions: function(mojit,binder){
+            var mojitAll = this._meta.mojits[mojit],
+                mojitmeta = mojitAll && mojitAll[binder || '*'],
+                selectors = mojitmeta && mojitmeta.meta.selectors.slice(),
+                matched = '';
+                if(selectors){
+                    matched = this._matchDimensions(selectors.pop(),this._ac.context,binder,mojitmeta.shaken);
+                }//selectorMeta?
+            return matched;
+        },
+        _getMojitRollup: function(mojitName,binder,sel){
+            try{
+                return this._meta.mojits[mojitName][binder || '*'].shaken[sel];
+            }catch(e){
+                console.log('ERR founding dimension for:' + mojitName);
+                return [];
+            }
+        },
+        _shakeMojits:function(mojits,mojitsDeps){
+            var binders = mojitsDeps.filter(function(i){return i.indexOf('/binders/');}),
+                shaken = [],
+                self = this;
+            mojits.forEach(function(mojit){
+                var binderName;
+                for(var i=0; i< mojitsDeps.length;i++){
+                    if(mojitsDeps[i].indexOf(mojit+'/binders/') !== -1){
+                        binderName = libpath.basename(mojitsDeps[i],'.js');
+                        break;
+                    }
+                }
+                selector = self._matchMojitDimensions(mojit,binderName);
+                mojitMatchedRollups = selector ? self._getMojitRollup(mojit,binderName,selector) : [];
+                shaken = shaken.concat(mojitMatchedRollups);
+            });
+            return shaken;
+        },
+        _getAppRollup: function(selector,action){
+            var app = this._meta.app,
+                shakedAction = app[action] || app['*'];
+
+            return shakedAction.shaken[selector].slice();
+        },
+        _shakeApp: function(appDeps){
+            var action = this._ac.action,
+                app = this._meta.app,
+                dimensions = this._ac.context,
+                shakenAction = app[action] || app['*'],
+                selectors = shakenAction.meta.selectors.slice(),
+                shaken = shakenAction.shaken,
+                selector = this._matchDimensions(selectors.pop(),dimensions,action,shaken);
+                return this._getAppRollup(selector,action);
+
+
+        },
+        shakeAll: function(meta){
+            var ac = this._ac;
+            var assets = ac.assets.getAssets(),
+                js = assets.top.js.concat(assets.bottom.js),
+                groups = filterAssets(this._app,js),
+                loadedMojits = [],rollupsMojits,rollupsApp;
+                for(var m in meta.children){
+                    var mojit = meta.children[m];
+                    loadedMojits.push(mojit.base || mojit.type );
+                }
+                rollupsMojits = this._shakeMojits(loadedMojits,groups.mojits),
+                rollupsApp = this._shakeApp(groups.app);
+                console.log(rollupsMojits);
+
+        },
+        shakeList: function(assets,type,location){
+
+        }
+    };
+
+    Y.mojito.addons.ac.shaker = ShakerAddon;
+
+}, '0.0.1', {requires: ['mojito']});
