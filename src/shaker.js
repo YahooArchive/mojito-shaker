c4d13501c1132abf82882a7998da0fe2d9e427ad
murphys <murphys@ea6c44dd-32d5-4dfe-a4a7-25d6a9a78781>
2012-03-28 21:48:56 +0000

    First pass of shaker command cleanup
    git-svn-id: svn+ssh://svn.corp.yahoo.com/yahoo/searchfe/shaker/trunk@47 ea6c44dd-32d5-4dfe-a4a7-25d6a9a78781

--- src/shaker.js
+++ src/shaker.js
@@ -9,13 +9,11 @@ var path = require('path'),
     Registry = require('buildy').Registry,
     Shaker = require('./lib/core').Shaker,
 
-    run,usage,options,
+    usage,
+    options,
+    run;
 
-/* GLOBAL */
-    MODULES_BAREBONES = [];
-
-
-usage =  "mojito compile {options} {type}\n" +
+usage =  "mojito shaker {options} {type}\n" +
        "\nOPTIONS: \n" +
        "\t --rollup-mojito     :  remove all compiled files, instead of creating them\n" +
        "\t  -rm                :  short for --rollup-mojito\n" +
@@ -53,42 +51,14 @@ options = [
     }
 ];
 
-function rollupMojito(params,options,callback){
-	var type = params.shift(),list;
-	if(!type) {
-		utils.error("Please provide the type of rollup you want.", exports.usage, true);
-		return;
-	}
-	switch(type){
-		case 'full': break;
-		case 'barebones': list = MODULES_BAREBONES ;break;
-		case 'modules' : list = ['modules']; break;
-		default: utils.error("Please provide a valid type of rollup.", exports.usage, true); return;
-	}
-	options.outputPath = options.output ? params.shift(): true;
-	options.type = type;
-	generateRollup(list,options,callback);
-
-}
-
-function generateRollup(list,options,callback){
+run = function(params, options, callback) {
 	var cwd = process.cwd(),
 		store = new ResourceStore(cwd),
-		dest = options.outputPath || cwd+'/artifacts/shaker',
-		fileName = 'rollup_'+ options.type + '.js',
-		modules,rolledModules = [],rollupBody = '',
-		context = {},
-		filterModules = function (item){
-			var moduleName = path.basename(i,'.js');
-			for(var i = 0; i < list.length; i++){
-				if(moduleName === list[item]) return true;
-			}
-			return false;
-		};
+		modules,rolledModules = [],rollupBody = '';
+
 	//get and filter the files from the store
 	store.preload();
-	var rollupMojito = (store.getRollupsApp('client', context)).srcs;
-	modules = list ? rollupMojito.filter(filterModules) : rollupMojito;
+	modules = store.getRollupsApp('client', {}).srcs;
 	for(var j = 0; j < modules.length; j++){
 		if(cwd === modules[j].substr(0, cwd.length)) {
             //skip the app level files (Note: to override path: substr(cwd.length + 1);)
@@ -98,37 +68,28 @@ function generateRollup(list,options,callback){
         rollupBody += fs.readFileSync(modules[j], 'utf-8');
     }
 
-    //output a file
-    if(options.output){
-			utils.makeDir(dest);
-			fs.writeFileSync(dest+'/'+fileName, rollupBody, 'utf-8');
-			callback();
-    }
-    //output inline
-    else{
-        //create mojito-core rollup
-        processRollup(rolledModules, 'assets/mojito/mojito_rollup_full', '.js', function(filename) {
-            transformedRollup(filename);
-        });
+    //create mojito-core rollup
+    processRollup(rolledModules, 'assets/mojito/mojito_rollup_full', '.js', function(filename) {
+        transformedRollup(filename);
+    });
 
-        //shaker stuff
-        var shaker = new Shaker({root: './'});
-        utils.log('[SHAKER] - Analizying application assets to Shake... ');
-        shaker.shakeAll(function(shaken){
-            if(options.stage || options.production){
-                compress(shaken,function(shaken_p){
-                    utils.log('[SHAKER] - Minifying and optimizing rollups... ');
-                    writeMetaData(shaken_p,callback);
-                });
-            }else{//dev
-                rename(shaken,function(shaken_r){
-                    utils.log('[SHAKER] - Processed assets for development env. ');
-                    writeMetaData(shaken_r,callback);
-                });
-            }
-        });
-    }
-}
+    var shaker = new Shaker({root: './'});
+    utils.log('[SHAKER] - Analizying application assets to Shake... ');
+    shaker.shakeAll(function(shaken){
+        if(options.stage || options.production){
+            compress(shaken,function(shaken_p){
+                utils.log('[SHAKER] - Minifying and optimizing rollups... ');
+                writeMetaData(shaken_p,callback);
+            });
+        }else{//dev
+            rename(shaken,function(shaken_r){
+                utils.log('[SHAKER] - Processed assets for development env. ');
+                writeMetaData(shaken_r,callback);
+            });
+        }
+    });
+};
+
 function rename(shaken,callback){
     var mojit,mojits,action,actions,dim,list,actionName,dimensions,item,
         app = path.basename(process.cwd());
@@ -235,18 +196,6 @@ function processRollup(files, name, ext, callback) {
 function buildShaker(params,options,callback){
 	require(process.cwd()+'/node_modules/shaker');
 }
-
-run = function(params, options, callback){
-	var context = {};
-	options = options || {};
-
-	switch(true){
-		case options['rollup-mojito'] :  rollupMojito(params,options,callback); break;
-		//case options.setup : setupShaker(params); break;
-		case options.build : buildShaker(); break;
-		default: utils.error("Please provide the type of compilation you want.", exports.usage, true);
-	}
-};
 
 exports.usage = usage;
 exports.options = options;
