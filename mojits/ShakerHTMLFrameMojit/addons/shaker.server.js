307d9163971e627a851dc6cf76ee3ced21252ea4
murphys <murphys@ea6c44dd-32d5-4dfe-a4a7-25d6a9a78781>
2012-04-20 18:34:10 +0000

    Added open source licensing

--- mojits/ShakerHTMLFrameMojit/addons/shaker.server.js
+++ mojits/ShakerHTMLFrameMojit/addons/shaker.server.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
 YUI.add('mojito-shaker-addon', function(Y, NAME) {
     var libfs = require('fs'),libpath = require('path');
 
