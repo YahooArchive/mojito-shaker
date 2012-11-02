338f6902dd566c7a6d9e8fbc36b78c03c923f540
Diego FV <elfogris@gmail.com>
2012-11-02 15:53:34 -0700

    fixed metadata fetch

--- addons/rs/shaker.server.js
+++ addons/rs/shaker.server.js
@@ -42,8 +42,9 @@ YUI.add('addon-rs-shaker', function(Y, NAME) {
             //this.onHostEvent('mojitResourcesResolved', this.mojitResourcesResolved, this);
 
             if (!this.initilized) {
-                this.meta = this.rs.config.readConfigJSON('shaker-meta.json');
+                this.meta = this.rs.config.readConfigJSON(libpath.join(this.appRoot, 'shaker-meta.json'));
             }
+
             if (this.meta) {
                 Y.log('Metadata loaded correctly.','info','Shaker');
             }else {
