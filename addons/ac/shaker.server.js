8efafe930c2adec569844cddc861682909458bc0
Daniel Vitor Morilha <dmorilha@gmail.com>
2013-02-14 15:45:45 -0800

    moving renderListAsHtmlAssets to the add-on, so other mojits can render their assets with no dependency on ShakerHTMLFrame

--- addons/ac/shaker.server.js
+++ addons/ac/shaker.server.js
@@ -172,7 +172,45 @@ YUI.add('mojito-shaker-addon', function (Y, NAME) {
                 routeFound = this.checkRouteBundling();
             }
             this.clientDeployment(meta);
+        },
+
+        renderListAsHtmlAssets: function (list, type) {
+            var i,
+                data = '',
+                url;
+
+            if ('js' === type) {
+                for (i = 0; i < list.length; i += 1) {
+                    // TODO: Fuly escape any HTML chars in the URL to avoid trivial
+                    // attribute injection attacks. See owasp-esapi reference impl.
+                    url = encodeURI(list[i]);
+                    data += '<script type="text/javascript" src="' +
+                        url + '"></script>\n';
+                }
+            } else if ('css' === type) {
+                for (i = 0; i < list.length; i += 1) {
+                    // TODO: Escape any HTML chars in the URL to avoid trivial
+                    // attribute injection attacks. See owasp-esapi reference impl.
+                    url = encodeURI(list[i]);
+                    data += '<link rel="stylesheet" type="text/css" href="' +
+                        url + '"/>\n';
+                }
+            } else if ('blob' === type) {
+                for (i = 0; i < list.length; i += 1) {
+                    // NOTE: Giant security hole...but used by everyone who uses
+                    // Mojito so there's not much we can do except tell authors of
+                    // Mojito applications to _never_ use user input to generate
+                    // blob content or populate config data. Whatever goes in here
+                    // can't be easily encoded without the likelihood of corruption.
+                    data += list[i] + '\n';
+                }
+            } else {
+                Y.log('Unknown asset type "' + type + '". Skipped.', 'warn', NAME);
+            }
+
+            return data;
         }
+
     };
 
     Y.mojito.addons.ac.shaker = ShakerAddon;
