7477362e1212b77b530fa063c6bf3e1ba8c64de4
jimenez <albert.louis.jimenez@gmail.com>
2014-10-03 17:26:27 -0700

    Updated tests.

--- test/apps/app1/mojits/Child/yui_modules/child-yui-module.client.js
+++ test/apps/app1/mojits/Child/yui_modules/child-yui-module.client.js
@@ -1,3 +1,4 @@
-YUI.add('child-yui-module', function(Y, NAME) {
+YUI.add('child-yui-module', function (Y, NAME) {
+    'use strict';
     Y.one(".log").append("<div>YUI: child-yui-module added</div>");
 }, '0.0.1', {requires: []});
